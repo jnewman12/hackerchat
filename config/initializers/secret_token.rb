84fd057e15f71b42ebb1c64054663f91931b7ecd
James Newman <jamesmnewman1@gmail.com>
2014-04-09 23:52:18 -0700

    set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackerchat::Application.config.secret_key_base = 'ba7d4c7962da2056d81c99b1b639d03f6ede126a151bd9a5885fe26cee77be20d87bf8c5b5957bd0965a4d8d17d3683fe340d529e3f4acd9ec4a528a9dea62ba'
